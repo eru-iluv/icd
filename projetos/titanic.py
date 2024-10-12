d060bff21879a1074fe32e3a656fe06aff669bcb
eru-iluv <edgardmacena@usp.br>
2024-10-11 22:45:55 -0300

    Titanic begin

---
+++ projetos/titanic.py
@@ -0,0 +1,363 @@
+import marimo
+
+__generated_with = "0.9.1"
+app = marimo.App(width="medium")
+
+
+@app.cell(hide_code=True)
+def __(mo):
+    mo.md(r"""# Projeto Titanic""")
+    return
+
+
+@app.cell(hide_code=True)
+def __(mo):
+    mo.md(
+        r"""
+        ## Preparação de terreno:
+
+        Nesta seção, são importados os packages que utilizaremos nos projetos.
+
+        Também criamos os `TypeAlias` que nos serão úteis e produzimos a paleta que será usada nos gráficos.
+        """
+    )
+    return
+
+
+@app.cell(hide_code=True)
+def __():
+    import marimo as mo
+    return (mo,)
+
+
+@app.cell
+def __():
+    import random
+    import pandas as pd
+    import numpy as np
+    import matplotlib.pyplot as plt
+    import matplotlib
+    from aquarel import load_theme
+    import seaborn as sns
+    import typing
+    from sklearn.preprocessing import StandardScaler
+    from sklearn.impute import KNNImputer
+    from sklearn.decomposition import PCA
+
+    theme = load_theme("boxy_light")
+    theme.apply()
+    # ... plotting code here
+    theme.apply_transforms()
+    return (
+        KNNImputer,
+        PCA,
+        StandardScaler,
+        load_theme,
+        matplotlib,
+        np,
+        pd,
+        plt,
+        random,
+        sns,
+        theme,
+        typing,
+    )
+
+
+@app.cell(hide_code=True)
+def __(matplotlib, pd):
+    DataFrame = pd.core.frame.DataFrame
+    Figure = matplotlib.figure.Figure
+    Axes: matplotlib.axes.Axes
+    return Axes, DataFrame, Figure
+
+
+@app.cell(hide_code=True)
+def __():
+    pallete_survived = {
+        0: "#e97777",
+        1: "#478c8c",
+    }
+    return (pallete_survived,)
+
+
+@app.cell
+def __(mo):
+    mo.md(r"""## Importação e transformação dos dados""")
+    return
+
+
+@app.cell
+def __(DataFrame, pd, random):
+    # Define a semente aleatória (Para termos resultados reproduzíveis)
+    random.seed(42)
+
+    # Importa os DataFrames train e test
+    train: DataFrame = pd.read_csv("data/titanic/train.csv", header=(0))
+    test: DataFrame = pd.read_csv("data/titanic/test.csv", header=(0))
+
+    print("Número de linhas e colunas no conjunto de treinamento:", train.shape)
+    print("Número de linhas e colunas no conjunto de teste:", test.shape)
+    attributes = list(train.columns)
+    train.head(10)
+
+    train_original = train
+
+    # Limpeza de colunas que não nos interessam
+    train.drop(
+        columns=[
+            "Cabin",
+            "Name",
+            "Ticket",
+            "PassengerId",
+        ],
+        inplace=True,
+    )
+
+    test.drop(
+        columns=[
+            "Cabin",
+            "Name",
+            "Ticket",
+            "PassengerId",
+        ],
+        inplace=True,
+    )
+
+
+    # Cria colunas que podem ser úteis
+    for df in [train, test]:
+        df["IsKid"] = df["Age"] < 18
+
+    train = pd.get_dummies(train, drop_first=True)
+    test = pd.get_dummies(test, drop_first=True)
+    return attributes, df, test, train, train_original
+
+
+@app.cell(hide_code=True)
+def __(mo):
+    mo.md(r"""Podemos observar os primeiros dados do treino.""")
+    return
+
+
+@app.cell
+def __(train):
+    train.head(20)
+    return
+
+
+@app.cell
+def __(mo):
+    mo.md(
+        r"""
+        Vamos converter os dados para matrizes de `numpy` e padronizá-los para facilitar a manipulação.
+
+        Ademais, usamos o `KNNImputer` para preenches os valores que estariam vazios.
+        """
+    )
+    return
+
+
+@app.cell
+def __(KNNImputer, StandardScaler, np, test, train):
+    data_train = train.to_numpy()
+    nrow, ncol = data_train.shape
+    y_train = data_train[:, 0]
+    y_train = y_train.astype(int)
+    X_train = data_train[:, 1:ncol]
+
+    data_test = test.to_numpy()
+    X_test = data_test
+
+    scaler = StandardScaler().fit(X_train)
+
+    X_train = scaler.transform(X_train)
+    X_test = scaler.transform(X_test)
+
+    imputer = KNNImputer(n_neighbors=4)
+
+    X_train = imputer.fit_transform(X_train)
+    X_test = imputer.transform(X_test)
+
+    print("Dados transformados:")
+    print("Media: ", np.mean(X_train, axis=0))
+    print("Desvio Padrao:", np.std(X_train, axis=0))
+
+    print("Dados transformados:")
+    print("Media: ", np.mean(X_test, axis=0))
+    print("Desvio Padrao:", np.std(X_test, axis=0))
+    return (
+        X_test,
+        X_train,
+        data_test,
+        data_train,
+        imputer,
+        ncol,
+        nrow,
+        scaler,
+        y_train,
+    )
+
+
+@app.cell(hide_code=True)
+def __(mo):
+    mo.md(r"""## Análise exploratória dos dados""")
+    return
+
+
+@app.cell(hide_code=True)
+def __(mo):
+    mo.md(r"""### Balanceamento das classes""")
+    return
+
+
+@app.cell
+def __(np, pallete_survived, plt, y_train):
+    _classes = y_train
+    cl = np.unique(_classes)
+    ncl = np.zeros(len(cl))
+    for i in np.arange(0, len(cl)):
+        a = _classes == cl[i]
+        ncl[i] = len(_classes[a])
+
+    numbers = np.arange(0, len(cl))
+    # Uma pequena dica visual do significado das classes
+    for _i in range(0,2):
+        plt.bar(numbers[_i], ncl[_i], alpha=0.75, color=pallete_survived[_i])
+
+    plt.xticks(numbers, cl)
+    plt.title("Número de elementos em cada classe")
+    plt.show()
+    return a, cl, i, ncl, numbers
+
+
+@app.cell(hide_code=True)
+def __(mo):
+    mo.md(r"""Vemos então que as classes estão mais ou menos balanceados. Podemos ter que prestar atenção nisso.""")
+    return
+
+
+@app.cell(hide_code=True)
+def __(mo):
+    mo.md(r"""### Correlação entre as varariáveis""")
+    return
+
+
+@app.cell
+def __(plt, train):
+    corr = train.corr()
+    # Plot Correlation Matrix using Matplotlib
+    plt.figure(figsize=(10, 8))
+    plt.imshow(corr, cmap="RdBu", interpolation="none", aspect="auto")
+    plt.colorbar()
+    plt.xticks(range(len(corr)), corr.columns, rotation="vertical")
+    plt.yticks(range(len(corr)), corr.columns)
+    plt.suptitle("Correlação entre variáveis", fontsize=18, fontweight="bold")
+    plt.grid(False)
+    plt.show()
+    return (corr,)
+
+
+@app.cell(hide_code=True)
+def __(mo):
+    mo.md(r"""Algumas variáveis são bem correlacionadas, mas, como ser criança ou não costuma a ser importante em procedimentos de resgate, e é interessante separar classe e tarifa, vamos mantê-las.""")
+    return
+
+
+@app.cell(hide_code=True)
+def __(mo):
+    mo.md(r"""### Relação entre gênero e sobrevivência.""")
+    return
+
+
+@app.cell
+def __(plt, sns, train_original):
+    sns.violinplot(
+        x="Sex", y="Age", hue="Survived", data=train_original, split=True
+    )
+    plt.title("Gráfico de violino entre gênero, idade e sobrevivência", fontsize=18, fontweight="bold", pad=20)
+    return
+
+
+@app.cell(hide_code=True)
+def __(mo):
+    mo.md(r"""Vemos que, para homens, existe uma relação muito forte entre idade e sobrevivência é muito forte. Já para mulheres, isso não é verdade.""")
+    return
+
+
+@app.cell
+def __(plt, sns, train_original):
+    group = train_original.groupby(["Pclass", "Survived"])
+    pclass_survived = group.size().unstack().astype(float)
+    for _i in range(1, 4):
+        pclass_survived.loc[_i] = (
+            pclass_survived.loc[_i] / pclass_survived.loc[_i].sum()
+        )
+    pclass_survived
+    plt.title(
+        "Porcentagem de sobreviventes por classe",
+        fontweight="bold",
+        fontsize=18,
+        pad=30,
+    )
+    sns.heatmap(pclass_survived, annot=True, fmt=".0%", cmap="PiYG")
+    return group, pclass_survived
+
+
+@app.cell
+def __(pd, sns, train_original):
+    # Divide Fare into 4 bins
+    train_original['Fare_Range'] = pd.qcut(train_original['Fare'], 4)
+     
+    # Barplot - Shows approximate values based on the height of bars.
+    sns.barplot(x ='Fare_Range', y ='Survived', hue="Fare_Range", data = train_original)
+
+    return
+
+
+@app.cell
+def __(pallete_survived, sns, train_original):
+    # Countplot
+    sns.catplot(x ='Embarked', hue ='Survived', palette=pallete_survived,
+    kind ='count', col ='Pclass', data = train_original)
+    return
+
+
+@app.cell(hide_code=True)
+def __(mo):
+    mo.md(r"""### Separação das classes""")
+    return
+
+
+@app.cell
+def __(PCA, X_train, np, plt, y_train):
+    pca = PCA(n_components=2)
+    pca_result = pca.fit_transform(X_train)
+
+    _classes = np.unique(y_train)
+
+    colors = ['b', 'g', 'r', 'c', 'm', 'y', 'k', 'w']
+    aux = 0
+    plt.figure(figsize=(8,5))
+    for c in _classes:
+        if c == 1:
+            lb = 'Sobreviveu'
+        else:
+            lb = 'Morreu'
+        nodes = np.where(y_train == c)
+        plt.scatter(pca_result[nodes,0], pca_result[nodes,1], s=50, 
+                    label = lb)
+        aux = aux + 1
+    plt.legend()
+    plt.xlabel("First component", fontsize=20)
+    plt.ylabel("Second component", fontsize=20)
+    plt.xticks(color='k', size=20)
+    plt.yticks(color='k', size=20)
+    plt.show()
+
+
+    return aux, c, colors, lb, nodes, pca, pca_result
+
+
+if __name__ == "__main__":
+    app.run()
--- quest/quest4/quest4.py
+++ quest/quest4/quest4.py
@@ -15,12 +15,29 @@ def __():
     import matplotlib.pyplot as plt
     from sklearn.metrics import r2_score
     from aquarel import load_theme
+    from sklearn.preprocessing import PolynomialFeatures
+    from sklearn.model_selection import GridSearchCV
+    from sklearn.neighbors import KNeighborsClassifier
+    from sklearn.linear_model import LogisticRegression
+    from sklearn.model_selection import cross_validate
+    from sklearn.metrics import accuracy_score
+    from sklearn.preprocessing import StandardScaler, LabelEncoder
+    from sklearn.model_selection import KFold
 
     theme = load_theme("boxy_light")
     theme.apply()
     # ... plotting code here
     theme.apply_transforms()
     return (
+        GridSearchCV,
+        KFold,
+        KNeighborsClassifier,
+        LabelEncoder,
+        LogisticRegression,
+        PolynomialFeatures,
+        StandardScaler,
+        accuracy_score,
+        cross_validate,
         lm,
         load_theme,
         mo,
@@ -59,14 +76,16 @@ def __(mo):
     return
 
 
-@app.cell(hide_code=True)
+@app.cell
 def __(pd, train_test_split):
-    df = pd.read_csv('data/BostonHousing.csv')
+    df = pd.read_csv("data/BostonHousing.csv")
     _data = df.to_numpy()
-    Xb = _data[:,:-1]
-    yb = _data[:,-1]
+    Xb = _data[:, :-1]
+    yb = _data[:, -1]
     p = 0.3
-    Xb_train, Xb_test, yb_train, yb_test = train_test_split(Xb, yb, test_size = p, random_state = 42)
+    Xb_train, Xb_test, yb_train, yb_test = train_test_split(
+        Xb, yb, test_size=p, random_state=42
+    )
     return Xb, Xb_test, Xb_train, df, p, yb, yb_test, yb_train
 
 
@@ -85,20 +104,22 @@ def __(Xb_test, Xb_train, lm, yb_train):
     return y_reg_pred,
 
 
-@app.cell(hide_code=True)
+@app.cell
 def __(np, plt, y_reg_pred, yb_test):
     _fig = plt.figure()
-    _l = plt.plot(y_reg_pred, yb_test, 'bo')
+    _l = plt.plot(y_reg_pred, yb_test, "bo")
     plt.setp(_l, markersize=10)
-    plt.setp(_l, markerfacecolor='C0')
+    plt.setp(_l, markerfacecolor="C0")
 
     plt.ylabel("y", fontsize=15)
     plt.xlabel("Prediction", fontsize=15)
 
     # mostra os valores preditos e originais
-    _xl = np.arange(min(yb_test), 1.2*max(yb_test),(max(yb_test)-min(yb_test))/10)
+    _xl = np.arange(
+        min(yb_test), 1.2 * max(yb_test), (max(yb_test) - min(yb_test)) / 10
+    )
     _yl = _xl
-    plt.plot(_xl, _yl, 'r--')
+    plt.plot(_xl, _yl, "r--")
 
     plt.show()
     return
@@ -111,66 +132,55 @@ def __(mo):
 
 
 @app.cell
-def __():
-    return
-
-
-@app.cell(hide_code=True)
-def __(mo):
-    alpha = mo.ui.slider(0.001, 1, value=1, step=0.1, label='alpha')
-
-    mo.md(
-        f"""
-        $\\alpha$
-
-        {alpha}
-        """
-    )
-    return alpha,
-
-
-@app.cell
-def __(Xb_test, Xb_train, alpha, lm, yb_train):
-    _model = lm.Lasso(alpha=alpha.value)
+def __(Xb_test, Xb_train, lm, yb_train):
+    _model = lm.Lasso(alpha=1)
     _model.fit(Xb_train, yb_train)
 
     y_lasso_pred = _model.predict(Xb_test)
     return y_lasso_pred,
 
 
-@app.cell(hide_code=True)
-def __(alpha, np, plt, y_lasso_pred, yb_test):
+@app.cell
+def __(np, plt, y_lasso_pred, yb_test):
     _fig = plt.figure()
-    _l = plt.plot(y_lasso_pred, yb_test, 'bo')
+    _l = plt.plot(y_lasso_pred, yb_test, "bo")
     plt.setp(_l, markersize=10)
-    plt.setp(_l, markerfacecolor='C0')
+    plt.setp(_l, markerfacecolor="C0")
 
     plt.ylabel("y", fontsize=15)
     plt.xlabel("Prediction", fontsize=15)
 
     # mostra os valores preditos e originais
-    _xl = np.arange(min(yb_test), 1.2*max(yb_test),(max(yb_test)-min(yb_test))/10)
+    _xl = np.arange(
+        min(yb_test), 1.2 * max(yb_test), (max(yb_test) - min(yb_test)) / 10
+    )
     _yl = _xl
-    plt.plot(_xl, _yl, 'r--')
-    plt.text(40,55,f"$\\alpha = {alpha.value}$", fontsize=12)
+    plt.plot(_xl, _yl, "r--")
+    plt.text(40, 55, f"$\\alpha = 1$", fontsize=12)
 
     plt.show()
     return
 
 
-@app.cell(hide_code=True)
+@app.cell
 def __(Xb_test, Xb_train, lm, np, plt, r2_score, yb_test, yb_train):
     _vR2 = []
     _valpha = []
-    for _alpha in np.arange(0.01,5,0.01):
-        _lasso = lm.Lasso(alpha = _alpha)
-        _lasso.fit(Xb_train, yb_train)             # Fit a ridge regression on the training data
-        _y_ridge_pred = _lasso.predict(Xb_test)           # Use this model to predict the test data
+    for _alpha in np.arange(0.01, 5, 0.01):
+        _lasso = lm.Lasso(alpha=_alpha)
+        _lasso.fit(
+            Xb_train, yb_train
+        )  # Fit a ridge regression on the training data
+        _y_ridge_pred = _lasso.predict(
+            Xb_test
+        )  # Use this model to predict the test data
         _r2 = r2_score(yb_test, _y_ridge_pred)
         _vR2.append(_r2)
         _valpha.append(_alpha)
-    plt.plot(_valpha, _vR2, '-r', ls='--')
-    plt.title("R2 entre $y$ e $\\hat{y}$ do método de Lasso\nem função de $\\alpha$")
+    plt.plot(_valpha, _vR2, "--r")
+    plt.title(
+        "R2 entre $y$ e $\\hat{y}$ do método de Lasso\nem função de $\\alpha$"
+    )
     plt.xlabel("$\\alpha$", fontsize=15)
     plt.ylabel("R2", fontsize=15)
     plt.show()
@@ -183,7 +193,7 @@ def __(mo):
     return
 
 
-@app.cell(hide_code=True)
+@app.cell
 def __(Xb_test, Xb_train, lm, yb_train):
     _model = lm.Ridge(alpha=0.2)
     _model.fit(Xb_train, yb_train)
@@ -192,39 +202,47 @@ def __(Xb_test, Xb_train, lm, yb_train):
     return y_ridge_pred,
 
 
-@app.cell(hide_code=True)
-def __(alpha, np, plt, y_ridge_pred, yb_test):
+@app.cell
+def __(np, plt, y_ridge_pred, yb_test):
     _fig = plt.figure()
-    _l = plt.plot(y_ridge_pred, yb_test, 'bo')
+    _l = plt.plot(y_ridge_pred, yb_test, "bo")
     plt.setp(_l, markersize=10)
-    plt.setp(_l, markerfacecolor='C0')
+    plt.setp(_l, markerfacecolor="C0")
 
     plt.ylabel("y", fontsize=15)
     plt.xlabel("Prediction", fontsize=15)
 
     # mostra os valores preditos e originais
-    _xl = np.arange(min(yb_test), 1.2*max(yb_test),(max(yb_test)-min(yb_test))/10)
+    _xl = np.arange(
+        min(yb_test), 1.2 * max(yb_test), (max(yb_test) - min(yb_test)) / 10
+    )
     _yl = _xl
-    plt.plot(_xl, _yl, 'r--')
+    plt.plot(_xl, _yl, "r--")
     plt.title("$y_ \\times \\hat{y}$\n Método de Ridge")
-    plt.text(40,55,f"$\\alpha = {alpha.value}$", fontsize=12)
+    plt.text(40, 55, f"$\\alpha = 1$", fontsize=12)
     plt.show()
     return
 
 
-@app.cell(hide_code=True)
+@app.cell
 def __(Xb_test, Xb_train, lm, np, plt, r2_score, yb_test, yb_train):
     _vR2 = []
     _valpha = []
-    for _alpha in np.arange(0.01,4,0.01):
-        _lasso = lm.Ridge(alpha = _alpha)
-        _lasso.fit(Xb_train, yb_train)             # Fit a ridge regression on the training data
-        _y_ridge_pred = _lasso.predict(Xb_test)           # Use this model to predict the test data
+    for _alpha in np.arange(0.01, 4, 0.01):
+        _lasso = lm.Ridge(alpha=_alpha)
+        _lasso.fit(
+            Xb_train, yb_train
+        )  # Fit a ridge regression on the training data
+        _y_ridge_pred = _lasso.predict(
+            Xb_test
+        )  # Use this model to predict the test data
         _r2 = r2_score(yb_test, _y_ridge_pred)
         _vR2.append(_r2)
         _valpha.append(_alpha)
-    plt.plot(_valpha, _vR2, '-r', ls='--')
-    plt.title("R2 entre $y$ e $\\hat{y}$ do método de Lasso\nem função de $\\alpha$")
+    plt.plot(_valpha, _vR2, "--r")
+    plt.title(
+        "R2 entre $y$ e $\\hat{y}$ do método de Lasso\nem função de $\\alpha$"
+    )
     plt.xlabel("$\\alpha$", fontsize=15)
     plt.ylabel("R2", fontsize=15)
     plt.show()
@@ -246,26 +264,432 @@ def __(mo):
 
 @app.cell(hide_code=True)
 def __(mo):
-    mo.md(r"""Podemos usar o fato de que a qualidade das predições piora com o aumento de $\alpha$ para escolher um valor adequado para o hiper parâmetro. No caso, usamos 2, onde temos uma mudança no declive da curva.""")
+    mo.md(r"""Podemos usar o fato de que a qualidade das predições piora com o aumento de $\alpha$ para escolher um valor adequado para o hiper parâmetro. No caso, usamos $0.2$, onde temos uma mudança no declive da curva.""")
     return
 
 
 @app.cell
 def __(Xb_train, lm, yb_train):
     # Fazemos a regressão
-    lasso = lm.Lasso(alpha = 0.2)
+    lasso = lm.Lasso(alpha=0.2)
     lasso.fit(Xb_train, yb_train)
 
     # Lemos quais são as váriaveis usadas
     _fileName = "data/BostonHousing.csv"
-    _fileHandle = open(_fileName, 'r')
+    _fileHandle = open(_fileName, "r")
     _cabeçalho = _fileHandle.readline()  # Lemos o cabeçalho
-    _cabeçalho = _cabeçalho.replace('"','')
+    _cabeçalho = _cabeçalho.replace('"', "")
     _fileHandle.close()
-    _cabeçalho = _cabeçalho.split(',')[:-1]
-    print("\n".join([f"{_cabeçalho[i]}: {lasso.coef_[i]:.2f}" for i in range(len(_cabeçalho))]))
+    _cabeçalho = _cabeçalho.split(",")[:-1]
+    print(
+        "\n".join(
+            [
+                f"{_cabeçalho[i]}: {lasso.coef_[i]:.2f}"
+                for i in range(len(_cabeçalho))
+            ]
+        )
+    )
     1
     return lasso,
+
+
+@app.cell(hide_code=True)
+def __(mo):
+    mo.md(
+        r"""
+        Vemos que os paramentros mais influentes são `rm` (número de comodos por habitação), `dis` (distância de centros de emprego) e `ptratio` (número de alunos por professor).
+
+        Dificilmente surpreendente. Como gostamos!
+        """
+    )
+    return
+
+
+@app.cell(hide_code=True)
+def __(mo):
+    mo.md(
+        r"""
+        ## Ex 3. 
+
+        Considere os dados gerados com o código a seguir. Usando regularização, ajuste o
+        grau do polinômio que define o modelo mais adequado.
+        """
+    )
+    return
+
+
+@app.cell
+def __(np, plt):
+    """Código do ex.3"""
+
+    np.random.seed(10)
+
+
+    # função para gerar os dados
+    def function(x):
+        y = x**4 + x**9
+        return y
+
+
+    # training set
+    N_train = 20
+    sigma = 0.2
+    x_train = np.linspace(0, 1, N_train)
+    y_train = function(x_train) + np.random.normal(0, sigma, N_train)
+    x_train = x_train.reshape(len(x_train), 1)
+    fig = plt.figure(figsize=(8, 4))
+    plt.scatter(
+        x_train,
+        y_train,
+        facecolor="blue",
+        edgecolor="b",
+        s=100,
+        label="training data",
+    )
+    # test set
+    N_test = 20
+    x_test = np.linspace(0, 1, N_test)
+    y_test = function(x_test) + np.random.normal(0, sigma, N_test)
+    x_test = x_test.reshape(len(x_test), 1)
+    # Curva teorica
+    xt = np.linspace(0, 1, 100)
+    yt = function(xt)
+    plt.plot(xt, yt, "-r", label="Theoretical curve")
+    plt.legend(fontsize=15)
+    plt.show()
+    return (
+        N_test,
+        N_train,
+        fig,
+        function,
+        sigma,
+        x_test,
+        x_train,
+        xt,
+        y_test,
+        y_train,
+        yt,
+    )
+
+
+@app.cell
+def __(PolynomialFeatures, lm, np, plt, x_test, x_train, y_test, y_train):
+    def rmse(y_true, y_pred):
+        return np.sqrt(np.mean(np.square(y_true - y_pred)))
+
+
+    poly15 = PolynomialFeatures(degree=15)
+    X_train = poly15.fit_transform(x_train)
+    X_test = poly15.transform(x_test)
+
+    _alphaV = np.arange(0.0002, 0.002, 0.0001)
+
+    rmseV = []
+
+    for _i in range(len(_alphaV)):
+        _alpha = _alphaV[_i]
+        _lasso = lm.Lasso(alpha=_alpha, max_iter=5000)
+        _lasso.fit(X_train, y_train)
+        _y_pred = _lasso.predict(X_test)
+        rmseV.append(rmse(y_test, _y_pred))
+    plt.figure()
+    plt.plot(_alphaV, rmseV, marker="o")
+    plt.xlabel("$\\alpha$", fontsize=12)
+    plt.ylabel("RMSE", fontsize=12)
+    plt.title("RMSE vs $\\alpha$", fontsize=18)
+    plt.show()
+    return X_test, X_train, poly15, rmse, rmseV
+
+
+@app.cell(hide_code=True)
+def __(mo):
+    mo.md(r"""Temos então que o menor erro ocorre para $\alpha = 0.009$. Usando esse valor, obtemos então os coeficientes""")
+    return
+
+
+@app.cell
+def __(X_test, X_train, lm, plt, x_test, xt, y_train, yt):
+    _alpha = 0.009
+    _lasso = lm.Lasso(alpha=_alpha, max_iter=5000)
+    _lasso.fit(X_train, y_train)
+    _y_pred = _lasso.predict(X_test)
+
+
+    lasso_coef = _lasso.coef_
+
+    _fig, _axs = plt.subplots(2, 1, figsize=(10, 10))
+    _axs[1].bar(range(len(lasso_coef)), lasso_coef)
+    _axs[1].set_xticks(range(len(lasso_coef)))
+    _axs[1].set_xlim([0, 10])
+
+    _axs[1].set_title("Lasso Regression Coefficients")
+
+    _axs[0].plot(xt, yt, "k:", label="Theoretical curve")
+    _axs[0].plot(x_test, _y_pred, "-r", label="Predicted curve")
+    _axs[0].legend(fontsize=15)
+
+    plt.show()
+    return lasso_coef,
+
+
+@app.cell(hide_code=True)
+def __(mo):
+    mo.md(r"""Vemos então que o programa resume os dois graus do polinômio em um único, de expoente 5.""")
+    return
+
+
+@app.cell(hide_code=True)
+def __(mo):
+    mo.md(
+        r"""
+        ## Ex 4.
+
+        Realize a classificação da base Vehicles usando validação cruzada e o método
+        grid_search para escolher os melhores hiperparâmetros do modelo regressão logística e
+        knn.
+        """
+    )
+    return
+
+
+@app.cell
+def __(LabelEncoder, StandardScaler, pd, train_test_split):
+    df_vehicle = pd.read_csv("data/Vehicle.csv")
+
+    vehicle = df_vehicle.to_numpy()
+    vehicle_y = vehicle[:, -1]
+    vehicle_X = vehicle[:, 0:-1]
+
+    # Pré processamento
+    v_scaler = StandardScaler()
+    v_scaler.fit(vehicle_X)
+    vehicle_X = v_scaler.transform(vehicle_X)
+
+    label_encoder = LabelEncoder()
+    vehicle_y = label_encoder.fit_transform(vehicle_y)
+
+    # Divisão dos train/test
+    vX_train, vX_test, vy_train, vy_test = train_test_split(
+        vehicle_X, vehicle_y, train_size=0.7, random_state=42
+    )
+    return (
+        df_vehicle,
+        label_encoder,
+        vX_test,
+        vX_train,
+        v_scaler,
+        vehicle,
+        vehicle_X,
+        vehicle_y,
+        vy_test,
+        vy_train,
+    )
+
+
+@app.cell(hide_code=True)
+def __(mo):
+    mo.md(
+        r"""
+        ### K-Vizinhos
+
+        Achando os melhores parâmetros
+        """
+    )
+    return
+
+
+@app.cell
+def __(GridSearchCV, KNeighborsClassifier, np, vX_train, vy_train):
+    _grid_params = {
+        "n_neighbors": np.arange(1, 10),
+        "weights": ["uniform", "distance"],
+        "metric": ["minkowski", "euclidean", "manhattan"],
+    }
+    _gs = GridSearchCV(
+        KNeighborsClassifier(), _grid_params, verbose=0, cv=5, n_jobs=-1
+    )
+    _g_res = _gs.fit(vX_train, vy_train)
+
+    print("Acurácia: ", _g_res.best_score_)
+    print("Melhores hiperparâmetros: ", _g_res.best_params_)
+    return
+
+
+@app.cell
+def __(
+    KNeighborsClassifier,
+    accuracy_score,
+    vX_test,
+    vX_train,
+    vy_test,
+    vy_train,
+):
+    _best_model = KNeighborsClassifier(
+        n_neighbors=7, metric="manhattan", weights="distance"
+    )
+
+    _best_model.fit(vX_train, vy_train)
+    _y_pred = _best_model.predict(vX_test)
+    print("Accuracy:", accuracy_score(_y_pred, vy_test))
+    return
+
+
+@app.cell(hide_code=True)
+def __(mo):
+    mo.md(
+        r"""
+        ### Regressão Logística
+
+        Achando os melhores parâmetros
+        """
+    )
+    return
+
+
+@app.cell
+def __(GridSearchCV, LogisticRegression, vX_train, vy_train):
+    _grid_params = {
+        "penalty": [None, "l2"],
+        "solver": ["saga", "lbfgs"],
+        "class_weight": [None, "balanced"],
+    }
+    _gs = GridSearchCV(
+        LogisticRegression(max_iter=1000),
+        _grid_params,
+        verbose=0,
+        cv=5,
+        n_jobs=-1,
+    )
+    _g_res = _gs.fit(vX_train, vy_train)
+
+
+    print("Acurácia: ", _g_res.best_score_)
+    print("Melhores hiperparâmetros: ", _g_res.best_params_)
+    return
+
+
+@app.cell
+def __(
+    LogisticRegression,
+    accuracy_score,
+    vX_test,
+    vX_train,
+    vy_test,
+    vy_train,
+):
+    _best_model = LogisticRegression(
+        solver="saga", class_weight=None, penalty=None, max_iter=5000
+    )
+    _best_model.fit(vX_train, vy_train)
+    _y_pred = _best_model.predict(vX_test)
+    print("Accuracy:", accuracy_score(_y_pred, vy_test))
+    return
+
+
+@app.cell(hide_code=True)
+def __(mo):
+    mo.md(
+        r"""
+        ## Ex 5.
+
+        Verifique se o número de folds, usado na validação cruzada, influencia na classificação
+        da base winequality-red. Use o modelo de regressão logística.
+        """
+    )
+    return
+
+
+@app.cell
+def __(pd, train_test_split, v_scaler):
+    df_wine = pd.read_csv("data/winequality-red.csv")
+
+    wine = df_wine.to_numpy()
+    wine_y = wine[:, -1]
+    wine_X = wine[:, 0:-1]
+
+    # Pré processamento
+    v_scaler.fit(wine_X)
+    wine_X = v_scaler.transform(wine_X)
+
+    # Divisão dos train/test
+    wX_train, wX_test, wy_train, wy_test = train_test_split(
+        wine_X, wine_y, train_size=0.7, random_state=42
+    )
+    return (
+        df_wine,
+        wX_test,
+        wX_train,
+        wine,
+        wine_X,
+        wine_y,
+        wy_test,
+        wy_train,
+    )
+
+
+@app.cell(hide_code=True)
+def __(GridSearchCV, LogisticRegression, wX_train, wy_train):
+    _grid_params = {
+        "penalty": [None, "l2"],
+        "solver": ["saga", "lbfgs"],
+        "class_weight": [None, "balanced"],
+    }
+    _gs = GridSearchCV(
+        LogisticRegression(max_iter=1000),
+        _grid_params,
+        verbose=0,
+        cv=5,
+        n_jobs=-1,
+    )
+    _g_res = _gs.fit(wX_train, wy_train)
+
+
+    print("Acurácia: ", _g_res.best_score_)
+    print("Melhores hiperparâmetros: ", _g_res.best_params_)
+    return
+
+
+@app.cell
+def __(
+    KFold,
+    LogisticRegression,
+    accuracy_score,
+    np,
+    wX_test,
+    wX_train,
+    wy_test,
+    wy_train,
+):
+    Ns = 10  # number of executions
+    mean_vacc = []
+    for _n in range(2, Ns + 1):
+        cv = KFold(n_splits=_n)
+        vacc = []
+        for train_index, test_index in cv.split(wX_train, wy_train):
+            _x_train, _x_test = wX_train[train_index], wX_train[test_index]
+            _y_train, _y_test = wy_train[train_index], wy_train[test_index]
+            _model = LogisticRegression(
+                solver="saga", penalty="l2", max_iter=1000
+            )
+            _model.fit(_x_train, _y_train)
+            _y_pred = _model.predict(wX_test)
+            _score = accuracy_score(_y_pred, wy_test)
+            vacc.append(_score)
+
+        mean_vacc.append(np.mean(vacc))
+    return Ns, cv, mean_vacc, test_index, train_index, vacc
+
+
+@app.cell
+def __(Ns, mean_vacc, plt):
+    plt.plot(range(2, Ns + 1), mean_vacc)
+    return
+
+
+@app.cell(hide_code=True)
+def __(mo):
+    mo.md(r"""Podemos ver que a influência do número de folds parece ser MUITO pequena. Da ordem da terceira casa decimal na acurácia.""")
+    return
 
 
 if __name__ == "__main__":
